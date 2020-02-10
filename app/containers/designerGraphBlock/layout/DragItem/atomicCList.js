440037cf51253e95f5534042d01c438d48769cf7
songbb <2521728972@qq.com>
2020-02-10 17:31:46 +0800

    fix:修改打开谷歌浏览器的原子能力数据结构

--- app/containers/designerGraphBlock/layout/DragItem/atomicCList.js
+++ app/containers/designerGraphBlock/layout/DragItem/atomicCList.js
@@ -25,14 +25,57 @@ const generateIcon = (type, style = DEFAULT_STYLE) => (
  */
 const abilityToDatastructure = {
   /** 启动chrome浏览器 */
+  // openBrowser: {
+  //   $$typeof: BasicStatementTag,
+  //   text: '启动chrome浏览器', // 代码的描述文字
+  //   module: 'selenium',
+  //   pkg: 'webdriver',
+  //   cmdName: '启动新的浏览器',
+  //   visible: '启动" chrome"浏览器，并将此浏览器作为控对象，赋值给hWeb',
+  //   main: 'Chrome',
+  //   output: 'hWeb',
+  //   outputDesc: '输出说明：返回是否启动成功',
+  //   cmdDesc: '命令说明、描述',
+  //   properties: {
+  //     required: [
+  //       {
+  //         cnName: '输出到',
+  //         enName: 'outPut',
+  //         value: 'hWeb',
+  //         default: 'hWeb',
+  //         componentType: 0,
+  //       },
+  //       {
+  //         cnName: '浏览器类型',
+  //         enName: 'executable_path',
+  //         value: "'C:\\chromedriver\\chromedriver.exe'",
+  //         default: "'C:\\chromedriver\\chromedriver.exe'",
+  //         desc: '属性说明',
+  //         paramType: '参数类型：0:变量，',
+  //         componentType: 1, //'组件类型:1：下拉框',
+  //         valueMapping: [
+  //           {
+  //             name: '谷歌chrome浏览器',
+  //             value: 'chrome',
+  //           },
+  //           {
+  //             name: '火狐浏览器',
+  //             value: 'fireFox',
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //     optional: [],
+  //   },
+  // },
   openBrowser: {
     $$typeof: BasicStatementTag,
-    text: '启动chrome浏览器', // 代码的描述文字
-    module: 'selenium',
-    pkg: 'webdriver',
+    text: '启动chrome浏览器',
+    module: 'sendiRPA',
+    pkg: 'browser',
     cmdName: '启动新的浏览器',
     visible: '启动" chrome"浏览器，并将此浏览器作为控对象，赋值给hWeb',
-    main: 'Chrome',
+    main: 'openBrowser',
     output: 'hWeb',
     outputDesc: '输出说明：返回是否启动成功',
     cmdDesc: '命令说明、描述',
@@ -45,14 +88,17 @@ const abilityToDatastructure = {
           default: 'hWeb',
           componentType: 0,
         },
+      ],
+      optional: [
         {
           cnName: '浏览器类型',
           enName: 'executable_path',
           value: "'C:\\chromedriver\\chromedriver.exe'",
           default: "'C:\\chromedriver\\chromedriver.exe'",
           desc: '属性说明',
-          paramType: '参数类型：0:变量，',
-          componentType: 1, //'组件类型:1：下拉框',
+          paramType: '参数类型：0: 变量，',
+          componentType: 1,
+          //'组件类型: 1：下拉框',
           valueMapping: [
             {
               name: '谷歌chrome浏览器',
@@ -64,8 +110,47 @@ const abilityToDatastructure = {
             },
           ],
         },
+        {
+          cnName: '执行前延时',
+          enName: 'delayBefore',
+          value: 1,
+          default: 1,
+          desc: '执行动作前延时x秒',
+          paramType: 0, //0: 数值,1: 字符串,2为布尔值
+          componentType: 0, //组件类型: 0: 输入框,1：下拉框
+          valueMapping: [],
+        },
+        {
+          cnName: '执行前延时',
+          enName: 'delayAfter',
+          value: 1,
+          default: 1,
+          desc: '执行动作前延时x秒',
+          paramType: 0, //0: 数值,1: 字符串,2为布尔值
+          componentType: 0, //组件类型: 0: 输入框,1：下拉框
+          valueMapping: [],
+        },
+        {
+          cnName: '执行失败是否继续',
+          enName: 'continue_On_Failure',
+          value: 'True',
+          default: 'True',
+          desc: '该步骤执行失败之后是否继续执行下一个操作',
+          paramType: 2, //0: 数值,1: 字符串,2为布尔值
+          componentType: 0,
+          //组件类型: 0: 输入框,1：下拉框
+          valueMapping: [
+            {
+              name: '是',
+              value: 'True',
+            },
+            {
+              name: '否',
+              value: 'False',
+            },
+          ],
+        },
       ],
-      optional: [],
     },
   },
   /** 鼠标双击截图 */
