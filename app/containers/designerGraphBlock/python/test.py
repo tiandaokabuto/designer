fb07b2209c5f9186689e269f803a038550e0aa60
songbb <2521728972@qq.com>
2020-02-14 15:25:45 +0800

    fix:测试发布流程

--- app/containers/designerGraphBlock/python/test.py
+++ app/containers/designerGraphBlock/python/test.py
@@ -1,7 +1,4 @@
 from sendiRPA import Browser
-from clickImage import MouseControl
 
-hWeb = Browser.openBrowser(driverPath = 'C:\\chromedriver\\chromedriver.exe')
-
-MouseControl.scshot_match(ImagePath = "C:\\Users\\Administrator.SC-201902012149\\Desktop\\template.jpg")
+hWeb = Browser.openBrowser(driverPath = "C:\\chromedriver\\chromedriver.exe")
 
