b71ef1d4d4554d60e779d5858c6cc8a75310c2e3
songbb <2521728972@qq.com>
2020-02-18 18:48:41 +0800

    fix:添加自定义组件

--- app/containers/designerGraphEdit/index.js
+++ app/containers/designerGraphEdit/index.js
@@ -7,6 +7,9 @@ import DragEditorHeader from '../common/DragEditorHeader';
 import GraphContainer from './layout/GraphContainer';
 import GraphItem from './layout/GraphItem';
 import GraphParamPanel from './layout/GraphParamPanel';
+import Image from '../images/icon.jpg';
+import ExecuteImage from '../images/execute.jpg';
+import EditImage from '../images/edit.jpg';
 
 import './index.scss';
 import { history } from '../../store/configureStore';
@@ -27,6 +30,120 @@ export default () => {
         <GraphItem />
         <GraphContainer />
         <GraphParamPanel />
+        <RegisterNode
+          name="model-card"
+          config={{
+            draw(item) {
+              const group = item.getGraphicGroup();
+              const model = item.getModel();
+              const width = 184;
+              const height = 56;
+              const x = -width / 2;
+              const y = -height / 2;
+              const borderRadius = 4;
+              const keyShape = group.addShape('rect', {
+                attrs: {
+                  x,
+                  y,
+                  width,
+                  height,
+                  radius: borderRadius,
+                  fill: 'white',
+                  stroke: '#CED4D9',
+                },
+              });
+
+              // 左侧色条
+              group.addShape('path', {
+                attrs: {
+                  path: [
+                    ['M', x, y + borderRadius],
+                    ['L', x, y + height - borderRadius],
+                    [
+                      'A',
+                      borderRadius,
+                      borderRadius,
+                      0,
+                      0,
+                      0,
+                      x + borderRadius,
+                      y + height,
+                    ],
+                    ['L', x + borderRadius, y],
+                    [
+                      'A',
+                      borderRadius,
+                      borderRadius,
+                      0,
+                      0,
+                      0,
+                      x,
+                      y + borderRadius,
+                    ],
+                  ],
+                  // fill: this.color_type,
+                },
+              });
+
+              // 类型 logo
+              group.addShape('image', {
+                attrs: {
+                  img: Image,
+                  x: x + 16,
+                  y: y + 20,
+                  width: 20,
+                  height: 16,
+                },
+              });
+
+              // 名称文本
+              const label = model.label ? model.label : '';
+
+              group.addShape('text', {
+                attrs: {
+                  text: label,
+                  x: x + 52,
+                  y: y + 22,
+                  textAlign: 'start',
+                  textBaseline: 'top',
+                  fill: 'rgba(0,0,0,0.65)',
+                },
+              });
+
+              // 状态 logo
+              group.addShape('image', {
+                attrs: {
+                  img: ExecuteImage,
+                  x: x + 200,
+                  y: y + 12,
+                  width: 16,
+                  height: 16,
+                  dataId: 'execute',
+                },
+              });
+              group.addShape('image', {
+                attrs: {
+                  img: EditImage,
+                  x: x + 200,
+                  y: y + 32,
+                  width: 16,
+                  height: 16,
+                  dataId: 'edit',
+                },
+              });
+
+              return keyShape;
+            },
+
+            // 设置锚点
+            anchor: [
+              [0.5, 0], // 上面边的中点
+              [0.5, 1], // 下边边的中点
+              [0, 0.5], // 左边的中点
+              [1, 0.5], // 右边的结点
+            ],
+          }}
+        />
       </GGEditor>
     </Fragment>
   );
--- app/containers/designerGraphEdit/layout/GraphContainer/index.js
+++ app/containers/designerGraphEdit/layout/GraphContainer/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Flow } from 'gg-editor';
+import { Flow, withPropsAPI } from 'gg-editor';
 
 import FlowItemPanel from './components/FlowItemPanel';
 
-export default () => {
+export default withPropsAPI(({ propsAPI }) => {
   return (
     <div className="designergraph-container">
       <div className="designergraph-container-header">
@@ -17,50 +17,22 @@ export default () => {
         onAfterChange={value => {
           // registerDataChange(value);
         }}
+        onNodeClick={_ => {
+          const { getSelected, executeCommand, update, save } = propsAPI;
+
+          const item = getSelected()[0];
+          console.log(item, save()); // 很重要
+          executeCommand(() => {
+            update(item, {
+              label: 'hhh',
+              style: {
+                fill: 'red',
+              },
+            });
+          });
+        }}
         noEndEdge={false}
-        // data={{
-        //   nodes: [
-        //     {
-        //       type: 'node',
-        //       size: '100*100',
-        //       shape: 'custom-node',
-        //       color: '#FA8C16',
-        //       label: 'Ant Design',
-        //       labelOffsetY: 20,
-        //       icon:
-        //         '//img.alicdn.com/tfs/TB1gXH2ywHqK1RjSZFPXXcwapXa-200-200.svg',
-        //       x: 100,
-        //       y: 100,
-        //       id: 'ea1184e8',
-        //       index: 0,
-        //     },
-        //     {
-        //       type: 'node',
-        //       size: '100*100',
-        //       shape: 'custom-node',
-        //       color: '#FA8C16',
-        //       label: 'React',
-        //       labelOffsetY: 20,
-        //       icon:
-        //         '//img.alicdn.com/tfs/TB1OzAmyyLaK1RjSZFxXXamPFXa-200-200.svg',
-        //       x: 100,
-        //       y: 300,
-        //       id: '481fbb1a',
-        //       index: 2,
-        //     },
-        //   ],
-        //   edges: [
-        //     {
-        //       source: 'ea1184e8',
-        //       sourceAnchor: 1,
-        //       target: '481fbb1a',
-        //       targetAnchor: 0,
-        //       id: '7989ac70',
-        //       index: 1,
-        //     },
-        //   ],
-        // }}
       />
     </div>
   );
-};
+});
