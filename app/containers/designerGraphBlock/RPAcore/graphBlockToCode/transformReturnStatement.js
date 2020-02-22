ea5ab40da0cf0cfb6a84dc59c5a07c1b99658ed5
songbb <2521728972@qq.com>
2020-02-22 11:29:43 +0800

    fix:解决冲突

--- app/containers/designerGraphBlock/RPAcore/graphBlockToCode/transformReturnStatement.js
+++ app/containers/designerGraphBlock/RPAcore/graphBlockToCode/transformReturnStatement.js
@@ -2,14 +2,6 @@ const transformReturnStatement = (padding, dataStructure, result) => {
   result.output += `${padding}`;
   const return_string = dataStructure['properties']['required'][0].value;
   result.output += `return ${return_string}`;
-<<<<<<< HEAD
-=======
-  // if (params) {
-  //   result.output += `print(${template_string}.${main_func}(${params}))`;
-  // } else {
-  //   result.output += `print(${template_string})`;
-  // }
->>>>>>> af58d69453e4d7a5def2d8fba22565019b595077
 };
 
 export default transformReturnStatement;
