e04cc67b865a9ee751c57bd5fd3624005b562b9c
songbb <2521728972@qq.com>
2020-03-09 11:14:20 +0800

    fix:对接设计器登录接口

--- app/api.js
+++ app/api.js
@@ -2,4 +2,7 @@ const context = 'http://172.168.201.90:19090/controller';
 
 export default {
   issueProcess: `${context}/designer/issueProcess`,
+  /** 设计器登录接口 */
+  signIn: `${context}/designerLogin/signIn`,
+  signOut: `${context}/designerLogin/signOut`,
 };
--- app/login/Login.js
+++ app/login/Login.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { Button, Input } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Button, Input, message } from 'antd';
+import axios from 'axios';
+
+import api from '../api';
+import { hex_sha1 } from './utils';
 
 const { ipcRenderer } = require('electron');
 
 import './login.scss';
 
 const Login = () => {
+  const [userName, setUserName] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+  const handleSignIn = () => {
+    axios
+      .post(api.signIn, {
+        userName: userName,
+        password: hex_sha1(password),
+      })
+      .then(json => {
+        if (~json.code) {
+          console.log('登录成功');
+        }
+        ipcRenderer.send('loginSuccess');
+      });
+  };
+  useEffect(() => {
+    axios.interceptors.response.use(response => {
+      // 如果存在返回码
+      if (response.data.code) {
+        if (response.data.code !== -1) {
+          // message.success(response.data.msg);
+        } else {
+          message.error(response.data.message);
+        }
+      }
+      return response.data;
+    });
+  }, []);
   return (
     <div className="login">
       <div
@@ -18,15 +50,26 @@ const Login = () => {
         <div className="login-right-title">欢迎使用RPA设计器</div>
         <div className="login-right-username">
           <div>用户名</div>
-          <Input placeholder="请输入用户名" />
+          <Input
+            placeholder="请输入用户名"
+            onChange={e => {
+              setUserName(e.target.value);
+            }}
+          />
         </div>
         <div className="login-right-password">
           <div>密码</div>
-          <Input placeholder="请输入密码" />
+          <Input
+            placeholder="请输入密码"
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
         </div>
         <Button
           onClick={() => {
-            ipcRenderer.send('loginSuccess');
+            handleSignIn();
+            // ipcRenderer.send('loginSuccess');
           }}
         >
           登录
