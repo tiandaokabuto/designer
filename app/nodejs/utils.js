457a85961e67901586ddc6516ebaa0a4028697f6
songbb <2521728972@qq.com>
2020-03-10 13:53:29 +0800

    fix:添加发布流程时的流程名设置

--- app/nodejs/utils.js
+++ app/nodejs/utils.js
@@ -1,15 +1,32 @@
 import axios from 'axios';
 import { message } from 'antd';
+import store from '../store';
 
+import { findNodeByKey } from '../containers/common/utils';
 import api from '../api';
 
 export const issueProcess = (content, descText) => {
+  const {
+    grapheditor: { currentCheckedTreeNode, processTree },
+  } = store.getState();
+
+  if (!currentCheckedTreeNode) {
+    return;
+  }
+
+  const node = findNodeByKey(processTree, currentCheckedTreeNode);
+  if (!node) return;
+
+  if (node.type !== 'process') {
+    message.info('流程未保存');
+    return;
+  }
+
   const file = new File([content], 'upload.zip', { type: 'zip' });
-  console.log(descText);
   const formData = new FormData();
   formData.append('file', file);
   formData.append('processId', '12345678');
-  formData.append('processName', '迭代2演示版本');
+  formData.append('processName', node.title);
   formData.append('desc', descText);
   formData.append('mainFile', 'test.py');
   axios
--- python/test.py
+++ python/test.py
@@ -1,4 +1,7 @@
-def RPA_b0f6706c(*argv, **kw):
+def RPA_356133a8(*argv, **kw):
+    from sendiRPA import Browser
+
+    hWeb = Browser.openBrowser(driverPath = "C:\\chromedriver\\chromedriver.exe")
 
 if __name__ == '__main__':
-    RPA_b0f6706c()
+    RPA_356133a8()
