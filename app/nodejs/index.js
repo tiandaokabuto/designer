b977dbaf4d096fc1fb7c6176c532f1ebb4b6f1f5
songbb <2521728972@qq.com>
2020-03-13 16:30:50 +0800

    fix:修改写入规则

--- app/nodejs/index.js
+++ app/nodejs/index.js
@@ -16,7 +16,7 @@ const writeFileRecursive = function(path, buffer, callback) {
   const lastPath = path.substring(0, path.lastIndexOf('/'));
   fs.mkdir(lastPath, { recursive: true }, err => {
     if (err) return callback(err);
-    fs.writeFile(path, buffer, function(err) {
+    fs.writeFile(path, buffer, { flag: 'w' }, function(err) {
       if (err) return callback(err);
       return callback(null);
     });
