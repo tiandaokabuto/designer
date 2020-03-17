b88f384db39f4d2b845e5d9f4d52c5081f87007f
伍俊杰 <wujj@gzsendi.cn>
2020-03-17 15:28:51 +0800

    'fix:添加打包后的python执行路径'

--- app/containers/common/DragEditorHeader/useHooks/useExecutePython.js
+++ app/containers/common/DragEditorHeader/useHooks/useExecutePython.js
@@ -3,11 +3,12 @@ import event, {
 } from '../../../designerGraphBlock/layout/eventCenter';
 const { exec } = require('child_process');
 const iconv = require('iconv-lite');
+const process = require('process');
 
 export default () => {
   return () => {
     event.emit('clear_output');
-    const worker = exec(`python ${process.cwd()}/python/temp.py`, {
+    const worker = exec(`${process.cwd()}\\Python\\Python37-32\\python.exe ${process.cwd()}/python/temp.py`, {
       encoding: 'buffer',
     });
     worker.stdout.on('data', function(data) {
