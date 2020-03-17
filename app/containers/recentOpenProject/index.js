cc491ee322897e1e3e2dcab7c3f72ab66336d23c
songbb <2521728972@qq.com>
2020-03-17 14:11:59 +0800

    fix:修改最近项目列表的头部样式

--- app/containers/recentOpenProject/index.js
+++ app/containers/recentOpenProject/index.js
@@ -5,6 +5,8 @@ import React, { useState, useMemo } from 'react';
 import { Table, Button, Input, message } from 'antd';
 import { useInjectContext } from 'react-hook-easier/lib/useInjectContext';
 
+import GraphBlockHeader from '../common/GraphBlockHeader';
+
 import {
   newProject,
   openProject,
@@ -40,55 +42,58 @@ export default useInjectContext(({ history }) => {
     return readAllFileName();
   }, []);
   return (
-    <div className="recentproject">
-      <div className="recentproject-leftcontent">
-        <div className="recentproject-leftcontent-newproject">
-          <Input
-            placeholder="请输入新建项目名称"
-            onChange={e => {
-              setName(e.target.value);
+    <div>
+      <GraphBlockHeader tag="recentProject" />
+      <div className="recentproject">
+        <div className="recentproject-leftcontent">
+          <div className="recentproject-leftcontent-newproject">
+            <Input
+              placeholder="请输入新建项目名称"
+              onChange={e => {
+                setName(e.target.value);
+              }}
+            />
+            <Button
+              type="primary"
+              onClick={() => {
+                if (!name) {
+                  message.info('请填写项目名称');
+                  return;
+                }
+                history.push('/designGraphEdit');
+                setTimeout(() => {
+                  newProject(name, () => {
+                    changeCurrentProject(name);
+                  });
+                }, 0);
+              }}
+            >
+              新建项目
+            </Button>
+          </div>
+          <h2>最近使用项目</h2>
+          <Table
+            columns={columns}
+            dataSource={fileList}
+            scroll={{
+              y: 'calc(100vh - 327px)',
+            }}
+            onRow={record => {
+              return {
+                onClick: event => {
+                  // 打开对应的项目
+                  openProject(record.name);
+                  changeCurrentProject(record.name);
+                  clearGrapheditorData();
+                  history.push('/designGraphEdit');
+                },
+              };
             }}
           />
-          <Button
-            type="primary"
-            onClick={() => {
-              if (!name) {
-                message.info('请填写项目名称');
-                return;
-              }
-              history.push('/designGraphEdit');
-              setTimeout(() => {
-                newProject(name, () => {
-                  changeCurrentProject(name);
-                });
-              }, 0);
-            }}
-          >
-            新建项目
-          </Button>
         </div>
-        <h2>最近使用项目</h2>
-        <Table
-          columns={columns}
-          dataSource={fileList}
-          scroll={{
-            y: 'calc(100vh - 327px)',
-          }}
-          onRow={record => {
-            return {
-              onClick: event => {
-                // 打开对应的项目
-                openProject(record.name);
-                changeCurrentProject(record.name);
-                clearGrapheditorData();
-                history.push('/designGraphEdit');
-              },
-            };
-          }}
-        />
-      </div>
-      <div className="recentproject-rightcontent">
-        <div className="recentproject-rightcontent-close"></div>
+        <div className="recentproject-rightcontent">
+          {/* <div className="recentproject-rightcontent-close"></div> */}
+        </div>
       </div>
     </div>
   );
