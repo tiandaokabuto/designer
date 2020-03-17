5e8fd05d43055f0f7129c155ffde9d2f89d20ac5
songbb <2521728972@qq.com>
2020-03-17 11:02:12 +0800

    fix:解决乱码问题

--- app/containers/common/DragEditorHeader/useHooks/useExecutePython.js
+++ app/containers/common/DragEditorHeader/useHooks/useExecutePython.js
@@ -2,19 +2,21 @@ import event, {
   PYTHON_OUTPUT,
 } from '../../../designerGraphBlock/layout/eventCenter';
 const { exec } = require('child_process');
-//const iconv = require('iconv-lite');
+const iconv = require('iconv-lite');
 
 export default () => {
   return () => {
     event.emit('clear_output');
-    const worker = exec(`python ${process.cwd()}/python/temp.py`);
+    const worker = exec(`python ${process.cwd()}/python/temp.py`, {
+      encoding: 'buffer',
+    });
     worker.stdout.on('data', function(data) {
-      console.log(data);
-      event.emit(PYTHON_OUTPUT, data);
+      const log = iconv.decode(data, 'cp936');
+      event.emit(PYTHON_OUTPUT, log);
     });
     worker.stderr.on('data', function(err) {
-      console.log(err);
-      event.emit(PYTHON_OUTPUT, err);
+      const log = iconv.decode(err, 'cp936');
+      event.emit(PYTHON_OUTPUT, log);
     });
   };
 };
