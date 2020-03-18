1a753c9fb715b7594eeb6acaf44ab31d45becf01
songbb <2521728972@qq.com>
2020-03-18 13:23:22 +0800

    fix:xx

--- app/containers/designerGraphBlock/layout/DragContainer/OutputPanel.js
+++ app/containers/designerGraphBlock/layout/DragContainer/OutputPanel.js
@@ -1,36 +1,35 @@
 import React, { useEffect, useState, memo } from 'react';
+import { useSelector } from 'react-redux';
 import useThrottle from 'react-hook-easier/lib/useThrottle';
 import event, { PYTHON_OUTPUT,  } from '../eventCenter';
 
 let isMouseDown = false;
 let startOffset = 0;
 
-export default memo(({ tag }) => {
-  const [output, setOutput] = useState('');
-  useEffect(() => {
-    const handleAnchorMouseMove = useThrottle(e => {
-      if (isMouseDown) {
-        let offset = startOffset - e.pageY;
-        startOffset = e.pageY;
-        if (e.clientY <= 114) return;
-        const outputDom = document.querySelector(
-          '.dragger-editor-container-output'
-        );
-        let originHeight = parseFloat(
-          window.getComputedStyle(outputDom).height
-        );
-        // if (originHeight <= 74 && offset < 0) return;
-        // originHeight = originHeight < 74 ? 74 : originHeight;
-        outputDom.style.height = originHeight + offset + 'px';
-      }
-    }, 0);
+export default memo(
+  useInjectContext(({ tag, updateExecuteOutput }) => {
+    const executeOutput = useSelector(
+      state => state.temporaryvariable.executeOutput
+    );
 
-    const handleMouseUp = () => {
-      isMouseDown = false;
-    };
-    document.addEventListener('mouseup', handleMouseUp);
-    document.addEventListener('mousemove', handleAnchorMouseMove);
-  }, []);
+    const [output, setOutput] = useState(executeOutput);
+    useEffect(() => {
+      const handleAnchorMouseMove = useThrottle(e => {
+        if (isMouseDown) {
+          let offset = startOffset - e.pageY;
+          startOffset = e.pageY;
+          if (e.clientY <= 114) return;
+          const outputDom = document.querySelector(
+            '.dragger-editor-container-output'
+          );
+          let originHeight = parseFloat(
+            window.getComputedStyle(outputDom).height
+          );
+          // if (originHeight <= 74 && offset < 0) return;
+          // originHeight = originHeight < 74 ? 74 : originHeight;
+          outputDom.style.height = originHeight + offset + 'px';
+        }
+      }, 0);
 
   useEffect(() => {
     const handlePythonOutput = stdout => {
@@ -50,26 +49,46 @@ export default memo(({ tag }) => {
     };
   }, []);
 
-  const style =
-    tag === 'graph'
-      ? {
-          position: 'fixed',
-          width: '100%',
-          bottom: '0px',
-          overflow: 'auto',
-          width: 'calc(100% - 478px)',
-        }
-      : {};
-  return (
-    <div className="dragger-editor-container-output" style={{ ...style }}>
-      <div
-        className="dragger-editor-container-output-anchor"
-        onMouseDown={e => ((isMouseDown = true), (startOffset = e.pageY))}
-      ></div>
-      <div>
-        输出:
-        <br /> <pre>{output}</pre>
+    useEffect(() => {
+      const handlePythonOutput = stdout => {
+        setOutput(output => output + '\n' + stdout);
+      };
+      const handleClearOutput = () => {
+        setOutput('');
+      };
+      event.addListener(PYTHON_OUTPUT, handlePythonOutput);
+      event.addListener('clear_output', handleClearOutput);
+      return () => {
+        event.removeListener(PYTHON_OUTPUT, handlePythonOutput);
+        event.removeListener('clear_output', handleClearOutput);
+      };
+    }, []);
+
+    useEffect(() => {
+      updateExecuteOutput(output);
+    }, [output]);
+
+    const style =
+      tag === 'graph'
+        ? {
+            position: 'fixed',
+            width: '100%',
+            bottom: '0px',
+            overflow: 'auto',
+            width: 'calc(100% - 478px)',
+          }
+        : {};
+    return (
+      <div className="dragger-editor-container-output" style={{ ...style }}>
+        <div
+          className="dragger-editor-container-output-anchor"
+          onMouseDown={e => ((isMouseDown = true), (startOffset = e.pageY))}
+        ></div>
+        <div>
+          输出:
+          <br /> <pre>{output}</pre>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  })
+);
