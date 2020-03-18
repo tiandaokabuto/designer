aad185b71ab313e1280c3ccb3351ffd00258aca1
songbb <2521728972@qq.com>
2020-03-18 13:01:33 +0800

    fix:添加参数的最长宽度限制

---
+++ python/test.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+def RPA_8943211d(*argv, **kw):
+
+    print("sdfdsaf")
+    print("sdfdsaf")
+    print("sdfdsaf")
+    print("sdfdsaf")
+    print("sdfdsaf")
+    print("sdfdsaf")
+    print("sdfdsaf")
+    print("sdfdsaf")
+    print("sdfdsaf")
+if __name__ == '__main__':
+    RPA_8943211d()
