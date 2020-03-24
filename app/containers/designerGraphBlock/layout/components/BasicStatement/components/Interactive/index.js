4eaf4e123e60532784ad9cfa2cdad9132e704842
songbb <2521728972@qq.com>
2020-03-24 13:30:02 +0800

    fix:xx

--- app/containers/designerGraphBlock/layout/components/BasicStatement/components/Interactive/index.js
+++ app/containers/designerGraphBlock/layout/components/BasicStatement/components/Interactive/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal } from 'antd';
 
 // import InteractiveWrapper from './components/InteractiveWrapper';
@@ -7,10 +7,20 @@ import WidgetPanel from './layout/WidgetPanel';
 
 import './index.scss';
 
-export default ({ visible, setVisible }) => {
+export default ({ visible, setVisible, interactiveCard }) => {
+  const [layout, setLayout] = useState(interactiveCard.layout || []);
   const onAddControl = item => {
     console.log('add control', item);
+    setLayout([
+      { i: 'a', x: 0, y: 1, w: 1, h: 2 },
+      { i: 'b', x: 1, y: 0, w: 3, h: 2 },
+      { i: 'c', x: 2, y: 0, w: 1, h: 2 },
+    ]);
   };
+
+  useEffect(() => {
+    interactiveCard.layout = layout;
+  }, [layout]);
   return (
     <Modal
       visible={visible}
@@ -30,7 +40,7 @@ export default ({ visible, setVisible }) => {
           <WidgetPanel onAddControl={onAddControl} />
         </div>
         <div className="interactive-container">
-          <InteractiveEditor />
+          <InteractiveEditor layout={layout} />
         </div>
         <div className="interactive-parampanel"></div>
       </div>
--- app/containers/designerGraphBlock/layout/components/BasicStatement/components/Interactive/layout/InteractiveEditor/index.js
+++ app/containers/designerGraphBlock/layout/components/BasicStatement/components/Interactive/layout/InteractiveEditor/index.js
@@ -7,13 +7,9 @@ import { useGetDomWidth } from '../../useHooks';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
-export default () => {
+export default ({ layout }) => {
   const [ref, width] = useGetDomWidth();
-  const layout = [
-    { i: 'a', x: 0, y: 1, w: 1, h: 2 },
-    { i: 'b', x: 1, y: 0, w: 3, h: 2 },
-    { i: 'c', x: 2, y: 0, w: 1, h: 2 },
-  ];
+
   const handleLayoutChange = layout => {
     console.log(layout);
   };
