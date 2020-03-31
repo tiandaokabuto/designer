3ee3dd4652f66ac19bc693fd2c00f040f84ee43a
songbb <2521728972@qq.com>
2020-03-31 09:41:56 +0800

    fix:修改登录的账号名称改为用户名称

--- app/login/Login.js
+++ app/login/Login.js
@@ -33,8 +33,8 @@ const Login = () => {
           key: 'name',
           inputValue: userName,
           handleInputVauleChange: setUserName,
-          label: '用户名',
-          placeholder: '请输入用户名',
+          label: '登录账号',
+          placeholder: '请输入登录账号',
           formItemClassName: 'login-right-username',
         },
         {
@@ -85,7 +85,7 @@ const Login = () => {
       .then(json => {
         if (json.code !== -1) {
           remote.getGlobal('sharedObject').token = json.data.token;
-          remote.getGlobal('sharedObject').userName = json.data.userName;
+          remote.getGlobal('sharedObject').userName = json.data.roleName;
           ipcRenderer.send('loginSuccess');
           return true;
         }
