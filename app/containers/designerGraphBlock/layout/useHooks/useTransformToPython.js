c649a8f0c8d3172ccef6d0f5acc344926aa0e59c
songbb <2521728972@qq.com>
2020-04-07 17:07:45 +0800

    fix:修改自定义变量的转译方式

--- app/containers/designerGraphBlock/layout/useHooks/useTransformToPython.js
+++ app/containers/designerGraphBlock/layout/useHooks/useTransformToPython.js
@@ -20,12 +20,24 @@ export default () => {
   );
   const currentCheckedTreeNodeRef = useRef(null);
   currentCheckedTreeNodeRef.current = currentCheckedTreeNode;
+
   const processTree = useSelector(state => state.grapheditor.processTree);
+
   const processTreeRef = useRef(null);
   processTreeRef.current = processTree;
+
+  const checkedGraphBlockId = useSelector(
+    state => state.grapheditor.checkedGraphBlockId
+  );
+  const graphDataMap = useSelector(state => state.grapheditor.graphDataMap);
+
+  const blockNode = graphDataMap.get(checkedGraphBlockId) || {};
+  const blockNodeRef = useRef(null);
+  blockNodeRef.current = blockNode;
+
   const handleEmitCodeTransform = useCallback(
     useDebounce(cards => {
-      const result = transformBlockToCode(cards);
+      const result = transformBlockToCode(cards, 0, blockNodeRef.current);
       if (transformCount && currentPagePositionRef.current === 'block') {
         changeModifyState(processTreeRef.current, currentCheckedTreeNode, true);
       }
