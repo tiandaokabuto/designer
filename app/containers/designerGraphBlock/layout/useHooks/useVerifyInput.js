c7cdd3740b50f231cd4c3b028c6ae8af7c4b5fb6
songbb <2521728972@qq.com>
2020-04-13 15:10:05 +0800

    fix:数值类型添加校验

---
+++ app/containers/designerGraphBlock/layout/useHooks/useVerifyInput.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import schema from 'async-validator';
+
+const numberDescriptor = {
+  value: {
+    type: 'number',
+    required: false,
+    validator: (rule, value) => /^[0-9]*$/g.test(value),
+  },
+};
+const NumberValidator = new schema(numberDescriptor);
+
+const VALIDATOR = {
+  NumberValidator,
+};
+
+const ERROR_MESSAGE = {
+  NumberValidator: '请填入数值类型',
+};
+
+const getValidType = param => {
+  const paramType = param.paramType;
+  if (
+    paramType === 0 ||
+    (Array.isArray(paramType) &&
+      paramType.length === 1 &&
+      paramType[0] === 'Number')
+  ) {
+    return 'NumberValidator';
+  }
+  return undefined;
+};
+
+export default param => {
+  const validType = getValidType(param);
+  const validator = VALIDATOR[validType];
+
+  const [err, setErr] = useState(false);
+  const [message, setMessage] = useState(ERROR_MESSAGE[validType]);
+
+  if (!validator) {
+    return [false, '', () => {}];
+  }
+
+  const handleValidate = data => {
+    validator.validate(data, errors => {
+      if (errors) {
+        setErr(true);
+      } else {
+        err && setErr(false);
+      }
+    });
+  };
+  return [err, message, handleValidate];
+};
