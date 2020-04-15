a65cd17b822ff01b4e16136fe411cadec0a82dfb
songbb <2521728972@qq.com>
2020-04-15 17:29:48 +0800

    fix:修改python的执行路径分

--- app/constants/localFilePath.js
+++ app/constants/localFilePath.js
@@ -3,8 +3,8 @@
 const FILE_PATH = {
   project: `${process.cwd()}/project/`,
   windowHook: `${process.cwd()}/../Python/python3_lib/python.exe ${process.cwd()}/../Python/python3_lib/Lib/site-packages/sendiRPA/testHook.py`,
-  //pythonExecute: `${process.cwd()}/../Python/python3_lib/python.exe ${process.cwd()}/python/temp.py`,
-  pythonExecute: `python3 ${process.cwd()}/python/temp.py`,
+  pythonExecute: `${process.cwd()}/../Python/python3_lib/python.exe ${process.cwd()}/python/temp.py`,
+  //pythonExecute: `python3 ${process.cwd()}/python/temp.py`,
 };
 
 export default (prefix, pendingPath) => {
