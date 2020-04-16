24243bd8ad338a055f238fae5556f588a9b3e522
songbb <2521728972@qq.com>
2020-04-16 14:50:52 +0800

    fix:排查bug

--- app/containers/designerGraphBlock/layout/useHooks/useWatchCmdDesc.js
+++ app/containers/designerGraphBlock/layout/useHooks/useWatchCmdDesc.js
@@ -4,7 +4,14 @@ export default card => {
   const [desc, setDesc] = useState(card._userDesc);
   useEffect(() => {
     const descriptor = Object.getOwnPropertyDescriptor(card, 'userDesc');
-    if (!card || card._userDesc || (descriptor && descriptor.get)) return;
+    if (!card) return;
+    if (descriptor && descriptor.get) {
+      Object.defineProperty(card, 'userDesc', {
+        get: null,
+        set: null,
+      });
+    }
+    console.log(card);
     card._userDesc = card.userDesc;
     Object.defineProperty(card, 'userDesc', {
       get() {
@@ -15,6 +22,6 @@ export default card => {
         setDesc(value);
       },
     });
-  }, []);
+  }, [card]);
   return desc;
 };
