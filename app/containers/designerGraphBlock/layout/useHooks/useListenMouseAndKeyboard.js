1da6b8e1a4fc4db7b4b44f7aa420bbccdb54db90
songbb <2521728972@qq.com>
2020-04-26 17:44:24 +0800

    fix:修改选中的问题

--- app/containers/designerGraphBlock/layout/useHooks/useListenMouseAndKeyboard.js
+++ app/containers/designerGraphBlock/layout/useHooks/useListenMouseAndKeyboard.js
@@ -191,6 +191,14 @@ export default () => {
     const win = remote.getCurrentWindow();
     if (!win || !win.webContents) return;
     electronLocalshortcut.register(win, 'Ctrl+C', () => {
+      const selected = window.getSelection().toString();
+      if (selected) {
+        updateClipBoardData({
+          dep: [],
+          content: undefined,
+        });
+        return;
+      }
       if (checkedId.length) {
         // 生成待保存的数据结构
         updateClipBoardData({
@@ -201,6 +209,9 @@ export default () => {
       }
     });
     electronLocalshortcut.register(win, 'Ctrl+V', () => {
+      if (!clipboardData.content) {
+        return;
+      }
       if (checkedId.length === 1) {
         // 生成待保存的数据结构
         const append = cloneDeep(clipboardData.content || []);
