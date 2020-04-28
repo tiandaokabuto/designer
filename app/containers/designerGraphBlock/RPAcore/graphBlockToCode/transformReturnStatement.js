02fbee0f2f33bfcd13120c81a8eb6a56665f1db1
songbb <2521728972@qq.com>
2020-04-28 12:05:18 +0800

    fix:开发一键恢复功能

--- app/containers/designerGraphBlock/RPAcore/graphBlockToCode/transformReturnStatement.js
+++ app/containers/designerGraphBlock/RPAcore/graphBlockToCode/transformReturnStatement.js
@@ -5,19 +5,14 @@ const transformReturnStatement = (
   blockNode = {}
 ) => {
   result.output += `${padding}`;
-<<<<<<< HEAD
   const returnList =
     (blockNode.properties &&
       blockNode.properties.find(item => item.enName === 'output').value) ||
     [];
-=======
-  const returnList = ''.find(item => item.enName === 'output').value || [];
-  // const returnList =
-  //   blockNode.properties.find(item => item.enName === 'output').value || [];
->>>>>>> f9474d3... fix:开发一键恢复功能
   const return_string = dataStructure['properties']['required'][0].value.map(
     item => item.name || 'None'
   );
+
   return_string.length = returnList.length;
   for (let i = 0; i < return_string.length; i++) {
     if (!return_string[i]) {
