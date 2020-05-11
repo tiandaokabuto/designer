9dd64cca85d7fd5100a8e67c0a6fc6bb144cb6ce
songbb <2521728972@qq.com>
2020-05-11 09:23:52 +0800

    fix:添加流程图转译部分的单元测试

---
+++ test/containers/designerGraphEdit/RPAcore/RPACore.spec.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('test RPA', () => {});
