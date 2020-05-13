434257ad1eb310718a061f7f28b845d783c6792d
songbb <2521728972@qq.com>
2020-05-13 15:08:02 +0800

    fix:添加reselect.spec.js的单元测试

--- test/containers/designerGraphBlock/RPAcore/reselect/reselect.spec.js
+++ test/containers/designerGraphBlock/RPAcore/reselect/reselect.spec.js
@@ -2,10 +2,10 @@ import memoize from '../../../../../app/containers/designerGraphBlock/RPAcore/gr
 
 describe('test memoize', () => {
   it('test memoize', () => {
-    const transformStatementImpl = jest.fn(
+    let transformStatementImpl = jest.fn(
       (padding, statement, result, moduleMap, options) => {}
     );
-    const transformStatement = memoize(transformStatementImpl);
+    let transformStatement = memoize(transformStatementImpl);
     let padding = '  ';
     let statement = { hasModified: false };
     let result = { output: '' };
@@ -25,6 +25,23 @@ describe('test memoize', () => {
     expect(transformStatementImpl.mock.calls.length).toBe(3);
     statement.hasModified = true;
     transformStatement(padding, statement, result, moduleMap, options);
+    expect(statement.hasModified).toBe(false);
     expect(transformStatementImpl.mock.calls.length).toBe(4);
+
+    transformStatementImpl = jest.fn(
+      (padding, statement, result, moduleMap, options) => {}
+    );
+    transformStatementImpl.mockReturnValue(['test', new Map()]);
+    transformStatementImpl.mockReturnValue([
+      'test',
+      new Map().set('name', 'xl'),
+    ]);
+    transformStatement = memoize(transformStatementImpl);
+    expect(
+      transformStatement(padding, statement, result, moduleMap, options)[0]
+    ).toBe('test');
+    transformStatement(padding, statement, result, moduleMap, options);
+    expect(moduleMap.get('name')).toBe('xl');
+    expect(result.output).toBe('test');
   });
 });
