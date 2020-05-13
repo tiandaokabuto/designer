31a221184bc0c9dd91e8cdd1e4d3b2e258e52549
songbb <2521728972@qq.com>
2020-05-13 14:12:17 +0800

    fix:修改模块导出配置

---
+++ test/containers/designerGraphBlock/RPAcore/reselect/reselect.spec.js
@@ -0,0 +1,7 @@
+import memoize from '../../../../../app/containers/designerGraphBlock/RPAcore/graphBlockToCode/reselect';
+
+describe('test memoize', () => {
+  it('test', () => {
+    expect(1).toBe(1);
+  });
+});
