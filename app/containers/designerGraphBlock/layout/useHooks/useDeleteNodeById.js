ddb4b9f812d9bb4d0c78e2bdd50c8d7bbad7faee
songbb <2521728972@qq.com>
2020-05-20 15:14:47 +0800

    fix:修改批量删除功能

--- app/containers/designerGraphBlock/layout/useHooks/useDeleteNodeById.js
+++ app/containers/designerGraphBlock/layout/useHooks/useDeleteNodeById.js
@@ -5,16 +5,16 @@ import { findNodeLevelById } from '../shared/utils';
 import useNoticyBlockCodeChange from './useNoticyBlockCodeChange';
 import { CHANGE_CARDDATA } from '../../../../actions/codeblock';
 
-export default id => {
-  const cards = useSelector(state => state.blockcode.cards);
-  const checkedId = useSelector(state => state.blockcode.checkedId);
+export default (id) => {
+  const cards = useSelector((state) => state.blockcode.cards);
+  const checkedId = useSelector((state) => state.blockcode.checkedId);
   const noticyChange = useNoticyBlockCodeChange();
   const dispatch = useDispatch();
   const handleDeleteNodeById = useCallback(
-    node_id => {
+    (node_id) => {
       const node_level = findNodeLevelById(cards, node_id);
       if (node_level) {
-        const index = node_level.findIndex(item => item.id === node_id);
+        const index = node_level.findIndex((item) => item.id === node_id);
         node_level.splice(index, 1);
         dispatch({
           type: CHANGE_CARDDATA,
@@ -26,18 +26,18 @@ export default id => {
     [cards, dispatch]
   );
 
-  useEffect(() => {
-    const handleDeleteKeyDown = e => {
-      if (e.keyCode === 46) {
-        if (checkedId.includes(id)) {
-          handleDeleteNodeById(id);
-        }
-      }
-    };
-    document.addEventListener('keydown', handleDeleteKeyDown);
-    return () => {
-      document.removeEventListener('keydown', handleDeleteKeyDown);
-    };
-  }, [handleDeleteNodeById, id, checkedId]);
+  // useEffect(() => {
+  //   const handleDeleteKeyDown = e => {
+  //     if (e.keyCode === 46) {
+  //       if (checkedId.includes(id)) {
+  //         handleDeleteNodeById(id);
+  //       }
+  //     }
+  //   };
+  //   document.addEventListener('keydown', handleDeleteKeyDown);
+  //   return () => {
+  //     document.removeEventListener('keydown', handleDeleteKeyDown);
+  //   };
+  // }, [handleDeleteNodeById, id, checkedId]);
   return handleDeleteNodeById;
 };
