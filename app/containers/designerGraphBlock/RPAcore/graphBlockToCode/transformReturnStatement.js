b0706da25f12f2beff139249d5492c69ce0bd205
伍俊杰 <wujj@gzsendi.cn>
2020-05-26 16:35:59 +0800

    feat:新增项目名特殊字符校验

--- app/containers/designerGraphBlock/RPAcore/graphBlockToCode/transformReturnStatement.js
+++ app/containers/designerGraphBlock/RPAcore/graphBlockToCode/transformReturnStatement.js
@@ -12,14 +12,16 @@ const transformReturnStatement = (
   blockNode = {},
   options
 ) => {
+  console.log(dataStructure);
+  console.log(blockNode);
   const ignore = dataStructure.ignore ? '# ' : '';
   result.output += `${padding}${ignore}`;
   const returnList =
     (blockNode.properties &&
-      blockNode.properties.find((item) => item.enName === 'output').value) ||
+      blockNode.properties.find(item => item.enName === 'output').value) ||
     [];
-  const return_string = dataStructure['properties']['required'][0].value.map(
-    (item) => item.name || 'None'
+  const return_string = dataStructure.properties.required[0].value.map(
+    item => item.name || 'None'
   );
 
   return_string.length = returnList.length;
@@ -28,6 +30,8 @@ const transformReturnStatement = (
       return_string[i] = 'None';
     }
   }
+  console.log(returnList);
+  console.log(return_string);
   result.output += `return ${return_string.join(', ')}`;
   return result.output;
 };
