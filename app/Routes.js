78a72928105e620dfaa5c3a2b78059dacf7d4583
伍俊杰 <wujj@gzsendi.cn>
2020-07-14 15:35:48 +0800

    fix: 增加BrowserRouter

--- app/Routes.js
+++ app/Routes.js
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 import Loading from './containers/images/loading.gif';
 import routes from '../app/constants/routes.json';
@@ -9,31 +10,33 @@ const DesignerGraph = lazy(() => import('./containers/designerGraph'));
 const RecentOpenProject = lazy(() => import('./containers/recentOpenProject'));
 
 export default () => (
-  <App>
-    <Suspense
-      fallback={
-        <div
-          style={{
-            width: '100%',
-            height: '100vh',
-            backgroundColor: 'white',
-          }}
-        >
-          <div className="loadingContent">
-            <img src={Loading} alt="loading" />
-            <p>正在加载主界面...</p>
+  <BrowserRouter>
+    <App>
+      <Suspense
+        fallback={
+          <div
+            style={{
+              width: '100%',
+              height: '100vh',
+              backgroundColor: 'white',
+            }}
+          >
+            <div className="loadingContent">
+              <img src={Loading} alt="loading" />
+              <p>正在加载主界面...</p>
+            </div>
           </div>
-        </div>
-      }
-    >
-      <Switch>
-        <Route
-          exact
-          path={routes.RecentOpenProject}
-          component={RecentOpenProject}
-        />
-        <Route path={routes.DESIGNGRAPH} component={DesignerGraph} />
-      </Switch>
-    </Suspense>
-  </App>
+        }
+      >
+        <Switch>
+          <Route
+            exact
+            path={routes.RecentOpenProject}
+            component={RecentOpenProject}
+          />
+          <Route path={routes.DESIGNGRAPH} component={DesignerGraph} />
+        </Switch>
+      </Suspense>
+    </App>
+  </BrowserRouter>
 );
