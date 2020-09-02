40026f97f602cf1ffa25fa3206ab6d67062d5f86
DEEPSEA\鲸 <liuq@gzsendi.cn>
2020-09-02 09:07:28 +0800

    fix: 修复chorme不能发送bug

--- app/main.dev.js
+++ app/main.dev.js
@@ -35,6 +35,21 @@ appexpress.use(bodyParser.urlencoded({ extended: false }));
 
 // 本地监听8888端口 获取动态的xpath元素回填
 
+
+appexpress.all('*', function(req, res, next) {
+  console.log(req.headers.origin)
+  console.log(req.environ)
+  //res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials","true");
+  res.header("X-Powered-By",' 3.2.1')
+  if(req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
+  else  next();
+});
+
+
 // --------------------------- express版本
 appexpress.post('/query', function (req, res) {
   if (targetId === undefined) {
